9b80b406c9dd7d5b3bec7f1a63d3475106b806a5
Lucxcuy <tkd1709@gmail.com>
2023-03-28 14:00:00 -0300

    Agrego colisiones, win y lose, vidas y timer

--- game.js
+++ game.js
@@ -1,8 +1,31 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
+const spanLives = document.querySelector("#lives");
+const spanTime = document.querySelector("#time");
 
 let canvasSize;
 let elementsSize;
+let level = 0;
+let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
+
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+};
+
+const giftPosition = {
+    x: undefined,
+    y: undefined,
+};
+let enemyPositions = [];
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -26,39 +49,164 @@ function startGame() {
     game.font = elementsSize + "px Verdana";
     game.textAlign = "center";
 
-    const map = maps[0];
+    const map = maps[level];
+
+    if (!map) {
+        gameWin();
+        return;
+    }
+
+    if (!timeStart) {
+        timeStart = Date.now();
+        timeInterval = setInterval(showTime, 100); //100 milisegundos
+    }
+
     const mapRows = map.trim().split("\n"); //split crea un array a partir de un string, en este caso cada vez que haya un salto de línea y Trim borra espacios en blanco innecesarios
     const mapAll = mapRows.map((row) => row.trim().split("")); //esto es un array ya, donde puedo acceder a todos los elementos
     console.log({ map, mapRows, mapAll });
+
+    showLives();
+
+    enemyPositions = [];
+    game.clearRect(0, 0, canvasSize, canvasSize);
 
     mapAll.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1);
             const posY = elementsSize * (rowI + 1);
+
+            if (col == "O") {
+                if (!playerPosition.x && !playerPosition.y) {
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log({ playerPosition });
+                    // console.log({ posX, posY });
+                }
+            } else if (col == "I") {
+                giftPosition.x = posX;
+                giftPosition.y = posY;
+            } else if (col == "X") {
+                enemyPositions.push({ x: posX, y: posY });
+            }
+
             game.fillText(emoji, posX, posY);
         });
-    }); // acá estamos recorriendo con foreach el array bidimensional a partir del string de nuestro mapa, y a partir del foreach estamos recibiendo dos parámetros que son la fila como tal, el array como tal y el indice de cada una de nuestras filas. a partir de esto estamos recorriendo este último array de filas para encontrar cada una de las columnas de cada fila y al índice de cada columna.
-    //Des´pues sacamos el emoji y las posiciones en X e Y las multiplicamos por el índice de cada elemento mas 1 porque debemos empezar en el indice 1 no el 0 pq sino los elementos se van a salir del canvas.
+    });
 
-    // for (let row = 1; row <= 10; row++) {
-    //     for (let col = 1; col <= 10; col++) {
-    //         game.fillText(
-    //             emojis[mapAll[row - 1][col - 1]],
-    //             elementsSize * col,
-    //             elementsSize * row
-    //         );
-    //     }
-    // } aca tambien podemos hacer el mapa pero es mas engorroso
+    movePlayer();
+}
 
-    //window.innerHeight; //tamaño de la pantalla para responsive
-    //window.innerWidth; //tamaño de la pantalla para responsive
+function movePlayer() {
+    const giftCollisionX =
+        playerPosition.x.toFixed(3) === giftPosition.x.toFixed(3);
+    const giftCollisionY =
+        playerPosition.y.toFixed(3) === giftPosition.y.toFixed(3);
+    const giftCollision = giftCollisionX && giftCollisionY;
 
-    // game.fillRect(0, 50, 100, 100); //creador de rectangulos
-    // game.clearRect(0, 0, 50, 50); //borrador de rectangulos
+    if (giftCollision) {
+        levelWin();
+    }
 
-    // game.font = "25px Verdana"; //modificar el fillText
-    // game.fillStyle = "purple"; //modificar el fillText
-    // game.textAlign = "end"; //modificar el fillText de manera que respete las medidas de el fillText
-    // game.fillText("Platzi", 50, 50); //agregar texto y ubicacion
+    const enemyCollision = enemyPositions.find((enemy) => {
+        const enemyCollisionX =
+            enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY =
+            enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    });
+
+    if (enemyCollision) {
+        levelFail();
+    }
+
+    game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function levelWin() {
+    console.log("Subiste de nivel!");
+    level++;
+    startGame();
+}
+
+function levelFail() {
+    console.log("Perdiste el nivel!");
+    lives--;
+
+    if (lives <= 0) {
+        level = 0;
+        lives = 3;
+        timeStart = undefined;
+    }
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
+    startGame();
+}
+
+function gameWin() {
+    console.log("Ganaste el juego!");
+    clearInterval(timeInterval);
+}
+
+function showLives() {
+    const heartsArray = Array(lives).fill(emojis["HEART"]); // [1,2,3]
+
+    spanLives.innerHTML = "";
+    heartsArray.forEach((heart) => spanLives.append(heart));
+}
+
+function showTime() {
+    spanTime.innerHTML = Date.now() - timeStart;
+}
+
+window.addEventListener("keydown", moveByKeys); //al llamar al adeventlistener sus respectivas funciones siempre van a recibir AL MENOS 1 PARÁMETRO
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+function moveByKeys(event) {
+    if (event.key === "ArrowUp") moveUp();
+    else if (event.key === "ArrowLeft") moveLeft();
+    else if (event.key === "ArrowRight") moveRight();
+    else if (event.key === "ArrowDown") moveDown();
+}
+function moveUp() {
+    console.log("Me quiero mober hacia arriba");
+
+    if (playerPosition.y - elementsSize < elementsSize) {
+        console.log("OUT");
+    } else {
+        playerPosition.y -= elementsSize;
+        startGame();
+    }
+}
+function moveLeft() {
+    console.log("Me quiero mober hacia la izquierda");
+    if (playerPosition.x - elementsSize < elementsSize) {
+        console.log("OUT");
+    } else {
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
+}
+function moveRight() {
+    console.log("Me quiero mober hacia la derecha");
+    if (playerPosition.x + elementsSize > canvasSize) {
+        console.log("OUT");
+    } else {
+        playerPosition.x += elementsSize;
+        startGame();
+    }
+}
+
+function moveDown() {
+    console.log("Me quiero mober hacia abajo");
+    if (playerPosition.y + elementsSize > canvasSize) {
+        console.log("OUT");
+    } else {
+        playerPosition.y += elementsSize;
+        startGame();
+    }
 }
